4e712586c21910f8fe01f0b351b72322a73264ab
ru5t3m <zharmagambetovrustem@gmail.com>
2023-05-11 22:44:05 +0600

    getKey

--- src/com/company/HashNode.java
+++ src/com/company/HashNode.java
@@ -4,4 +4,14 @@ public class HashNode<K, V> {
     private K key;
 
     private V value;
+
+    public HashNode(K key, V value){
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey(){
+        return key;
+    }
+
 }
