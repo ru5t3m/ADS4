3f67ac1ccf194a575ed6f4d16c5ce0687f6b0290
ru5t3m <zharmagambetovrustem@gmail.com>
2023-05-17 17:24:19 +0600

    HashTable(int)

--- src/com/company/HashTable.java
+++ src/com/company/HashTable.java
@@ -10,6 +10,10 @@ public class HashTable<K, V> {
         size = 0;
         chain = new LinkedList[M];
     }
-
+    public HashTable(int m) {
+        size = 0;
+        this.M = m;
+        chain = new LinkedList[M];
+    }
 
 }
