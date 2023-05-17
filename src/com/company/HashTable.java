f405102cccf26251d8e8baebee6b00eeaede66d0
ru5t3m <zharmagambetovrustem@gmail.com>
2023-05-17 17:23:37 +0600

    HashTable

--- src/com/company/HashTable.java
+++ src/com/company/HashTable.java
@@ -6,7 +6,10 @@ public class HashTable<K, V> {
     private LinkedList<HashNode<K, V>>[] chain;
     private int M = 11;
     private int size;
-
+    public HashTable() {
+        size = 0;
+        chain = new LinkedList[M];
+    }
 
 
 }
